14527ba746b3882d4f2d9d139b56a03d221395fe
AnaJazVazquez27 <vazquezanajazmin27@gmail.com>
2023-08-24 18:23:22 -0300

    ultima modificación

---
+++ registrarse.js
@@ -0,0 +1,10 @@
+const miBoton = document.getElementById('miBoton');
+
+miBoton.addEventListener('click', () => {
+    Swal.fire({
+        title: '¡Bien!',
+        text: 'Usurio registrado con éxito',
+        icon: 'success',
+        confirmButtonText: 'Ok'
+    });
+});
