281ee2ee6f8109172f7c5645f521f75f16a33c5f
Jorge Caballero <jorge.caballero.31.10.98@gmail.com>
2023-08-24 19:05:23 -0300

    Correccion script registro

--- registrarse.js
+++ registrarse.js
@@ -1,10 +1,11 @@
-const miBoton = document.getElementById('miBoton');
+const miBoton = document.getElementById("miBoton");
 
-miBoton.addEventListener('click', () => {
-    Swal.fire({
-        title: '¡Bien!',
-        text: 'Usurio registrado con éxito',
-        icon: 'success',
-        confirmButtonText: 'Ok'
-    });
+miBoton.addEventListener("click", () => {
+  Swal.fire({
+    title: "¡Bien!",
+    text: "Usurio registrado con éxito",
+    icon: "success",
+    confirmButtonText: "Ok",
+  });
+  return false;
 });
